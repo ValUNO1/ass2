1fe7a45ea17a489089d120a54c9bd8e0bfd360bb
ValUNO1 <93164480+ValUNO1@users.noreply.github.com>
2022-03-02 19:14:02 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e86cmdl3vutr1v8u8q4#^ol@u^wv-7gdrjlt%rk(okuz&wh@)c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
